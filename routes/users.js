f571810a268d2154cf3eab75e7929f94f098203a
SravyaVannemreddy <143159657+SravyaVannemreddy@users.noreply.github.com>
2023-10-25 19:19:44 -0500

    changed the code

--- routes/users.js
+++ routes/users.js
@@ -1,14 +1,12 @@
 var express = require('express');
 var router = express.Router();
-
 // Initializing the variables
 var sum = 0;
 var more = 0;
-
-// Define the router
+// Defining the router
 router.get('/', function(req, res, next) {
   more += 1;
-  // Add 'more' to 'sum'
+  // Adding 'more' to 'sum'
   sum += more;
   res.send(`Sum is: ${sum}`);
 });
